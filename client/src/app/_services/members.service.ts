8990833beab69b257188b43c4bf7d8b2ed4dbf25
Endryu <ipodendriuk@gmail.com>
2022-11-11 11:11:26 +0000

    members service

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/member';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    //Authorization: 'Bearer' + JSON.parse(localStorage.getItem('user')).token
+    // this portion of the lecture is depricated, this needs fixing. Look through the Q and A section and find the answer
+    Authorization:'Bearer '+ JSON.parse(localStorage.getItem('user')?localStorage.getItem('user')!:"{}").token
+  })
+  
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+  }
+}
